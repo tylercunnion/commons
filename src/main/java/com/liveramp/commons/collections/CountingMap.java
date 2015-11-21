ce2a3aa1e936ba48926ead42c75f71670822c461
Ben Podgursky <bpodgursky@liveramp.com>
2015-11-20 16:04:41 -0800

    trivial incrementing map wrapper class

---
+++ src/main/java/com/liveramp/commons/collections/CountingMap.java
@@ -0,0 +1,29 @@
+package com.liveramp.commons.collections;
+
+import java.util.Map;
+
+import com.google.common.collect.Maps;
+
+public class CountingMap<T> {
+  private final Map<T, Long> map;
+
+  public CountingMap(){
+    this(Maps.<T, Long>newHashMap());
+  }
+
+  public CountingMap(Map<T, Long> backing){
+    this.map = backing;
+  }
+
+  public void increment(T key, Long amount){
+    if(!map.containsKey(key)){
+      map.put(key, 0L);
+    }
+    map.put(key, map.get(key)+amount);
+  }
+
+  public Map<T, Long> get(){
+    return map;
+  }
+
+}
